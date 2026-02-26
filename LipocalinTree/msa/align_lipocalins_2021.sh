c1bfe93e5c917183c338324ea01921c2237fc041
Tomas Jelinek <tomgolf.jelinek@gmail.com>
2026-02-26 11:25:04 +0100

    add files

---
+++ LipocalinTree/msa/align_lipocalins_2021.sh
@@ -0,0 +1,1 @@
+mafft --localpair --maxiterate 1000 ../data/mouse_lipocalins_2021.fasta >mouse_lipocalins_2021_aligned.fasta
---
+++ LipocalinTree/tree/make_tree.sh
@@ -0,0 +1,1 @@
+iqtree3 -s ../msa/mouse_lipocalins_2021_aligned.fasta -st AA -m MFP -bb 1000 -alrt 1000 -nt AUTO
