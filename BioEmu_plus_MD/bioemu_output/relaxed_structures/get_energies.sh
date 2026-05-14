4cf7bec223b829cfb03b17ef9fbd6ea88b260667
Tomáš Jelínek <73285347+Desperadus@users.noreply.github.com>
2026-05-14 10:26:48 +0200

    Update xtb command path in get_energies.sh

--- BioEmu_plus_MD/bioemu_output/relaxed_structures/get_energies.sh
+++ BioEmu_plus_MD/bioemu_output/relaxed_structures/get_energies.sh
@@ -2,6 +2,6 @@
 echo "File, Energy" >>gfnff_en_summary.csv
 for f in *_H.pdb; do
   echo "Calculating $f"
-  energy=$(/home/tomgolf/.local/bin/xtb-dist/bin/xtb "$f" --gfnff --alpb water | grep "total energy" | awk '{print $4}')
+  energy=$(xtb "$f" --gfnff --alpb water | grep "total energy" | awk '{print $4}')
   echo "$f, $energy" >>gfnff_en_summary.csv
 done
